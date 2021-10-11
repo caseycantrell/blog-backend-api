9db4d06d3d8977fb157abb25c59dfce3cbc051cb
Casey <cantrellcasey@gmail.com>
2021-10-11 16:40:43 -0500

    updates for front-end actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
+  validates :password, length: { in: 6..20 }
+  validates :name, length: { in: 2..50 }
 
   has_many :posts
 end
